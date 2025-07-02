5b76c17021507794854f66c2ac2db89695122c03
Alberto Garcia <u6052747@CMLT-0724-1225.mws.csbs.utah.edu>
2025-07-02 17:33:49 -0600

    age-based VSL using aldy and viscusi

--- scripts/12-age_VSL.R
+++ scripts/12-age_VSL.R
@@ -1,15 +1,45 @@
 
+income_elasticity_mort = 0.4
 
-# Start w/ VSL (2024)
-VSL_24 = 13.24189
+age_vsl_data <- fread("./Aldy & Viscusi 2008 Figure 1 Data.csv", stringsAsFactors = F)%>%
+  mutate(age_vsl_2000 = vslcohort3,
+         age_min  = age, 
+         age_max = ifelse(age<62,age,100))%>%
+  select(age_min,age_max,age_vsl_2000)%>%
+  mutate(year=2024)
 
-# discount rate
-r = 0.03
+## Load real GDP per cap growth rates
 
-median_age_utah = 31.7
-median_life_expectancy_utah = 77.2
+gdp_growth <- fread("./realGDPpercapita_growth.csv", stringsAsFactors = F)%>%
+  drop_na(GDP_per_cap_2017)%>%
+  mutate(growth_rate = rGDPpc,
+         year = as.integer(str_sub(observation_date,1,4)))%>%
+  select(year,growth_rate)
 
-L_a = median_life_expectancy_utah - median_age_utah
+growth_rates <- gdp_growth %>%
+  filter(year>2000)%>%
+  mutate(cum_growth = cumprod(1 + growth_rate))
 
-VSLY_24 = (r*VSL_24)/(1-(1+r)^(-L_a))
+## Combine
 
+age_vsl_2019 <- age_vsl_data%>%
+  left_join(growth_rates, by = c("year" = "year"))%>%
+  mutate(
+    age_VSL_2024 = future_WTP(
+      income_elasticity_mort,
+      (cum_growth - 1),
+      age_vsl_2000)*1.484654)
+
+age_vsl_2024 %>%
+  summarise(age_vsl_2024 = mean(age_vsl_2024))
+
+age_vsl_2024 %>%
+  ggplot(aes(x=age_min, y = age_vsl_2024/1000000))+
+  geom_point()+
+  geom_line()+
+  theme_cowplot(16)+
+  labs(x= "Age", y = "2024 Cohort-Adjusted VSL (million 2024 $) from Aldy & Viscusi (2008)")
+
+age_vsl_2024%>%
+  select(age_min, age_max,year,age_vsl_2024)%>%
+  write.csv("processed/age_based_VSL_2019.csv", row.names = F)
