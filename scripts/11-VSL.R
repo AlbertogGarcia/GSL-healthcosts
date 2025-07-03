545d9ef63acc790d64d790fb41a873ba6925c8e3
Alberto Garcia <u6052747@CMLT-0724-1225.mws.csbs.utah.edu>
2025-07-02 20:57:55 -0600

    using per capita income for vsl adjustment

--- scripts/11-VSL.R
+++ scripts/11-VSL.R
@@ -1,3 +1,24 @@
+# GSL dust costs: VSL adjustment
+# albert.garcia@utah.edu
+# created: 06/20/2025
+# updated: 
+
+# Set up environment ########################################
+
+# load or install necessary libraries. 
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(measurements,  # convert units easily
+               tidyverse, # tidyverse
+               stringr,
+               fuzzyjoin,
+               ggplot2
+)
+
+options(scipen=999)  # turn off sci notation
+options(dplyr.summarise.inform = FALSE)  # turn off dplyr group by comments
+options(java.parameters = "-Xmx8000m") 
+`%ni%` <- Negate(`%in%`)  # "not in" function
+
 
 
 # CPI in 2006 and 2024
@@ -6,11 +27,24 @@ CPI_2024 <- 132.34870
 CPI_2006 <- 85.05367
 CPI_0624 <- CPI_2024/CPI_2006
 
-# Real GDP in 2006 and 2024
+# Real GDP per capita in 2006 and 2024
 # https://fred.stlouisfed.org/series/GDPCA
-GDP_2024 <- 23305.023
-GDP_2006 <- 16433.148
-growth_0624 <- GDP_2024/GDP_2006
+# GDP_2024 <- 23305.023
+# GDP_2006 <- 16433.148
+# growth_0624 <- GDP_2024/GDP_2006
+
+gdp_growth <- read.csv("data/health/realGDPpercapita_growth.csv", stringsAsFactors = F)%>%
+  drop_na(rGDPpc)%>%
+  mutate(growth_rate = rGDPpc/100,
+         year = ifelse(str_sub(observation_date,-2,-1) > 25, paste0("19", str_sub(observation_date,-2,-1)), paste0("20", str_sub(observation_date,-2,-1)))
+  )%>%
+  select(year,growth_rate)
+
+growth_rates <- gdp_growth %>%
+  filter(year>2006)%>%
+  mutate(cum_growth = cumprod(1 + growth_rate))
+
+growth_0624 <- growth_rates %>% filter(year == 2024) %>% select(cum_growth) %>% pull()
 
 # US EPA recommended VSL (2013 dollars) and income elasticity  
 VSL_2006 <- 7.4
@@ -23,7 +57,7 @@ income_elasticity <- 0.4
 VSL_24 = VSL_2006*CPI_0624*growth_0624^income_elasticity
 
 # How does that compare to simply adjusting for inflation
-# VSL_24_inflationonly = VSL_2006*CPI_0624
+VSL_24_inflationonly = VSL_2006*CPI_0624
 
 # Compare to Thivierge's code
 # VSL_test = (income_elasticity * (growth_0624-1) * VSL_2006 + VSL_2006)*CPI_0624
--- scripts/12-age_VSL.R
+++ scripts/12-age_VSL.R
@@ -1,45 +1,74 @@
+# GSL dust costs: Mortality and population projections
+# albert.garcia@utah.edu
+# created: 06/20/2025
+# updated: 
 
-income_elasticity_mort = 0.4
+# Set up environment ########################################
 
-age_vsl_data <- fread("./Aldy & Viscusi 2008 Figure 1 Data.csv", stringsAsFactors = F)%>%
+# load or install necessary libraries. 
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(measurements,  # convert units easily
+               tidyverse, # tidyverse
+               stringr,
+               fuzzyjoin,
+               ggplot2,
+               cowplot
+)
+
+options(scipen=999)  # turn off sci notation
+options(dplyr.summarise.inform = FALSE)  # turn off dplyr group by comments
+options(java.parameters = "-Xmx8000m") 
+`%ni%` <- Negate(`%in%`)  # "not in" function
+
+
+# CPI in 2006 and 2024
+# (https://fred.stlouisfed.org/series/CPALTT01USA661S)
+CPI_2024 <- 132.34870
+CPI_2000 <- 72.65302
+CPI_0024 <- CPI_2024/CPI_2000
+
+income_elasticity = 0.4
+
+age_vsl_data <- read.csv("data/health/mortality/Aldy_Viscusi_2008_Fig1Data.csv", stringsAsFactors = F)%>%
   mutate(age_vsl_2000 = vslcohort3,
-         age_min  = age, 
+         age_min  = ifelse(age > 18,age,0), 
          age_max = ifelse(age<62,age,100))%>%
   select(age_min,age_max,age_vsl_2000)%>%
   mutate(year=2024)
 
 ## Load real GDP per cap growth rates
 
-gdp_growth <- fread("./realGDPpercapita_growth.csv", stringsAsFactors = F)%>%
-  drop_na(GDP_per_cap_2017)%>%
-  mutate(growth_rate = rGDPpc,
-         year = as.integer(str_sub(observation_date,1,4)))%>%
+gdp_growth <- read.csv("data/health/realGDPpercapita_growth.csv", stringsAsFactors = F)%>%
+  drop_na(rGDPpc)%>%
+  mutate(growth_rate = rGDPpc/100,
+         year = ifelse(str_sub(observation_date,-2,-1) > 25, paste0("19", str_sub(observation_date,-2,-1)), paste0("20", str_sub(observation_date,-2,-1)))
+         )%>%
   select(year,growth_rate)
 
-growth_rates <- gdp_growth %>%
-  filter(year>2000)%>%
-  mutate(cum_growth = cumprod(1 + growth_rate))
+growth_0024 <- gdp_growth %>%
+  filter(year > 2000) %>%
+  mutate(year = as.character(year),
+         cum_growth = cumprod(1 + growth_rate)) %>% 
+  filter(year == 2024) %>% 
+  select(cum_growth) %>% 
+  pull()
 
 ## Combine
 
-age_vsl_2019 <- age_vsl_data%>%
-  left_join(growth_rates, by = c("year" = "year"))%>%
-  mutate(
-    age_VSL_2024 = future_WTP(
-      income_elasticity_mort,
-      (cum_growth - 1),
-      age_vsl_2000)*1.484654)
+age_vsl_2024 <- age_vsl_data%>%
+  mutate(age_vsl_2024 = age_vsl_2000*CPI_0024*growth_0024^income_elasticity)
 
 age_vsl_2024 %>%
   summarise(age_vsl_2024 = mean(age_vsl_2024))
 
 age_vsl_2024 %>%
-  ggplot(aes(x=age_min, y = age_vsl_2024/1000000))+
-  geom_point()+
+  mutate(age_min = ifelse(age_min == 0, 18, age_min))%>%
+  ggplot(aes(x=age_min, y = age_vsl_2024))+
+  geom_point(size = 0.75)+
   geom_line()+
-  theme_cowplot(16)+
+  theme_classic(16)+
   labs(x= "Age", y = "2024 Cohort-Adjusted VSL (million 2024 $) from Aldy & Viscusi (2008)")
 
-age_vsl_2024%>%
-  select(age_min, age_max,year,age_vsl_2024)%>%
+age_vsl_2024 %>%
+  select(age_min, age_max,year,age_vsl_2024) %>%
   write.csv("processed/age_based_VSL_2019.csv", row.names = F)
